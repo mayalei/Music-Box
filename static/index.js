9375072235bc1c2553870ff75f72260ad1ebe041
student <student@MAC3-23.local>
2016-05-05 15:26:25 -0700

    index html files modified

---
+++ static/index.js
@@ -0,0 +1,68 @@
+
+$(document).ready( function() {
+    
+    // This controls the button for the c note.
+    
+    // This variable calls the cNote audio element in the HTML.
+    var cNote = document.getElementById('cAudio');
+
+    // This turns our <div id= "c"> into a button that plays our audio file when the box is pressed.
+    $('#c').mousedown(function(){
+        
+        // This is a property that scrubs the audio file back to its start.
+        cNote.currentTime = 0;
+        
+        // This plays the audio file.
+        cNote.play();
+    });
+    
+    // This controls the button for the d note
+    
+    $('#d').mousedown(function(){
+        dNote.currentTime = 0;
+        dNote.play();
+    });
+    
+    // This controls the button for the e note.
+    
+    $('#e').mousedown(function(){
+        dNote.currentTime = 0;
+        dNote.play();
+    });
+    
+ 
+    // This controls the button for the f note.
+    $('#f').mousedown(function(){
+        dNote.currentTime = 0;
+        dNote.play();
+    });
+    
+    
+    
+    // This controls the button for the g note.
+    $('#g').mousedown(function(){
+        dNote.currentTime = 0;
+        dNote.play();
+    });
+    
+    
+    
+    // This controls the button for the a note.
+    $('#a').mousedown(function(){
+        dNote.currentTime = 0;
+        dNote.play();
+    });
+    
+    
+    
+    // This controls the button for the b note.
+    $('#b').mousedown(function(){
+        dNote.currentTime = 0;
+        dNote.play();
+    });
+    
+    
+    
+});
+
+
